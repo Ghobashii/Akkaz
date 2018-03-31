076251676a91d07d1765af5aa12f60040e02f10c
Mohamed Ghobashy <mohamedghobashii@gmail.com>
2018-03-31 19:53:04 +0200

    initial commit

---
+++ Akkaz/src/main/java/com/SweProject/Akkaz/AkkazApplication.java
@@ -0,0 +1,12 @@
+package com.SweProject.Akkaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkkazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkkazApplication.class, args);
+	}
+}
